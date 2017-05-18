19767e2b3cbac7ed957f5d2b35dc9e8f2036857e
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2017-05-17 18:22:58 -0700

    implement *rateAtDate funcs

--- rates_test.go
+++ rates_test.go
@@ -24,6 +24,25 @@ func TestStandardRate(t *testing.T) {
 	}
 }
 
+func TestStandardRateAtDate(t *testing.T) {
+	rate, ok := StandardRateAtDate("EL", since(2017, 5, 15))
+	if !ok {
+		t.Error("should give rate for EL")
+	}
+	if rate != 24 {
+		t.Errorf("wrong rate for EL since 2017-5-15: %v", rate)
+	}
+
+	rate, ok = StandardRateAtDate("EL", since(2016, 12, 30))
+	if !ok {
+		t.Error("should give rate for EL")
+	}
+	if rate != 23 {
+		t.Errorf("wrong rate for EL since 2016-12-30: %v", rate)
+	}
+
+}
+
 func TestIsEUCountry(t *testing.T) {
 	var tests = []struct {
 		countryCode string
